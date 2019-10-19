884504534f829866d791948962de2b0ba6e83db7
Andrey_Lukashenko <andrei_lukashenko@ukr.net>
2019-10-19 23:05:59 +0300

    Updates

---
+++ precache-manifest.5e87d79b7b051f99f544cc5439832f6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe38853134a6b3ded74d89f62ceb97de",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "bb3257135fe2b1dcf1ba",
+    "url": "/social/static/css/2.eeb25080.chunk.css"
+  },
+  {
+    "revision": "0f3ce976f4667aacbe1f",
+    "url": "/social/static/css/main.f3bd55c0.chunk.css"
+  },
+  {
+    "revision": "bb3257135fe2b1dcf1ba",
+    "url": "/social/static/js/2.33edccac.chunk.js"
+  },
+  {
+    "revision": "0f3ce976f4667aacbe1f",
+    "url": "/social/static/js/main.558eb517.chunk.js"
+  },
+  {
+    "revision": "29fe826fb10aec9feb55",
+    "url": "/social/static/js/runtime~main.4069bf5c.js"
+  },
+  {
+    "revision": "f0b4985c3b000d0e02d68f6ece5ca3f5",
+    "url": "/social/static/media/banner_img.f0b4985c.svg"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/social/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "1df4dc0dced5ed9e61a77c92780d4c71",
+    "url": "/social/static/media/loader.1df4dc0d.svg"
+  },
+  {
+    "revision": "cc40b773d356af9efa2030fd9b900967",
+    "url": "/social/static/media/logo.cc40b773.svg"
+  }
+]);
